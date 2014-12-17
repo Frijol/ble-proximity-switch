cfb0581c91697ee5e90043ed50336f5d4c2db34d
Kelsey Breseman <ifoundthemeaningoflife@gmail.com>
2014-12-16 20:23:21 -0800

    authorizes and deauthorizes based on presence of approved devices

--- index.js
+++ index.js
@@ -7,20 +7,60 @@ var ble = blelib.use(tessel.port['A']);
 
 // Set authorized device
 acceptedDevices = [
-  "105.105.60.123.235.203"
+  '105.105.60.123.235.203'
 ];
 
+// Local vars
+var noneFound; // var name for timeout
+var authorized = false;
+
+// Initial scan for devices
 ble.on('ready', function(err) {
-  console.log('Scanning...');
-  ble.startScanning();
+  scan();
 });
 
+// When a device is discovered
 ble.on('discover', function(peripheral) {
-  deviceID = peripheral.address._str.toString();
-  console.log("Discovered peripheral!", deviceID);
+  deviceID = peripheral.address._str;
   if(acceptedDevices.indexOf(deviceID) > -1) {
-    console.log('Device accepted.');
+    console.log('Authorized device in range.', deviceID);
+    ble.stopScanning();
+    clearTimeout(noneFound);
+    if(!authorized) {
+      authorize();
+    }
+    // Check for changes
+    poll();
   } else {
-    console.log('Unrecognized device.');
+    console.log('Unrecognized device discovered.', deviceID);
   }
 });
+
+// Scan for devices regularly
+function poll() {
+  setTimeout(scan, 5000);
+}
+
+// Check and see if authed devices in range
+function scan () {
+  console.log('Scanning...');
+  ble.startScanning();
+  noneFound = setTimeout(function () {
+    console.log('No authorized BLE devices in range.');
+    if(authorized) {
+      deauthorize();
+    }
+    // Check for changes
+    poll();
+  }, 5000);
+}
+
+function authorize () {
+  authorized = true;
+  console.log('authorized:', authorized);
+}
+
+function deauthorize () {
+  authorized = false;
+  console.log('authorized:', authorized);
+}
